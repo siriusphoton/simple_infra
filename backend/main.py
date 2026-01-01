ea02665cb9d623f37c8a9466e00e38788c6084b9
Nidimineni Sree Sai <sreesai072@gmail.com>
2026-01-01 10:59:07 +0000

    added frontend and stopped tracking pycache

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
+import os
 from fastapi import FastAPI
+import psycopg2
 
 app = FastAPI()
 
+conn = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+)
+
 @app.get("/")
 def root():
-    return {"message": "Hello from backend"}
+    cur = conn.cursor()
+    cur.execute("SELECT NOW();")
+    result = cur.fetchone()
+    cur.close()
+    return {"db_time": str(result[0])}
